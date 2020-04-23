5806e535a5a5afce093d9f682b2c77fdeae039eb
Aronne Brivio <aronne.brivio@gmail.com>
2020-04-23 11:04:13 +0200

    JS refactoring

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function setCookie(c_name, value, exdays) {
-  var exdate = new Date()
-  exdate.setDate(exdate.getDate() + exdays)
-  var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString())
-  document.cookie = c_name + "=" + c_value
+function setCookie(name, value, daysToLive) {
+  var expirationDate = new Date()
+  expirationDate.setDate(expirationDate.getDate() + daysToLive)
+  var completeValue = escape(value) + ((daysToLive == null) ? '' : '; expires=' + expirationDate.toUTCString())
+  document.cookie = name + '=' + completeValue
 }
 
-function getCookie(c_name) {
-  var i, x, y, ARRcookies = document.cookie.split(";")
-  for (i = 0; i < ARRcookies.length; i++) {
-    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="))
-    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1)
-    x = x.replace(/^\s+|\s+$/g, "")
-    if (x == c_name) {
+function getCookie(name) {
+  var i, x, y, cookies = document.cookie.split(';')
+  for (i = 0; i < cookies.length; i++) {
+    x = cookies[i].substr(0, cookies[i].indexOf('='))
+    y = cookies[i].substr(cookies[i].indexOf('=') + 1)
+    x = x.replace(/^\s+|\s+$/g, '')
+    if (x == name) {
       return unescape(y)
     }
   }
