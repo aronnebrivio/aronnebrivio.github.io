088bd7697843d5d3c3950778c02d54e33eea1f1e
Aronne Brivio <aronne.brivio@gmail.com>
2020-05-03 14:39:50 +0200

    Moved source files to src/ folder

--- build.sh
+++ build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-echo "Cleaning up..."
-rm -rf dist/
-mkdir dist
-mkdir dist/styles
-
+SRC_FOLDER="src"
 MAIN_FILE_NAME="index"
 PALETTE="styles/palette.css"
 THEME="styles/theme.css"
 FAVICON="favicon.png"
 PROFILE="profile.jpg"
 
+echo "Cleaning up..."
+rm -rf dist/
+mkdir dist
+mkdir dist/styles
+
 echo "Building..."
-./node_modules/.bin/html-minifier --collapse-whitespace $MAIN_FILE_NAME.html -o dist/$MAIN_FILE_NAME.html
-./node_modules/.bin/uglifyjs --compress --mangle -o dist/$MAIN_FILE_NAME.js -- $MAIN_FILE_NAME.js
-./node_modules/.bin/cssnano $PALETTE dist/$PALETTE
-./node_modules/.bin/cssnano $THEME dist/$THEME
-./node_modules/.bin/cssnano $MAIN_FILE_NAME.css dist/$MAIN_FILE_NAME.css
-cp $FAVICON dist/$FAVICON
-cp $PROFILE dist/$PROFILE
+./node_modules/.bin/html-minifier --collapse-whitespace $SRC_FOLDER/$MAIN_FILE_NAME.html -o dist/$MAIN_FILE_NAME.html
+./node_modules/.bin/uglifyjs --compress --mangle -o dist/$MAIN_FILE_NAME.js -- $SRC_FOLDER/$MAIN_FILE_NAME.js
+./node_modules/.bin/cssnano $SRC_FOLDER/$PALETTE dist/$PALETTE
+./node_modules/.bin/cssnano $SRC_FOLDER/$THEME dist/$THEME
+./node_modules/.bin/cssnano $SRC_FOLDER/$MAIN_FILE_NAME.css dist/$MAIN_FILE_NAME.css
+cp $SRC_FOLDER/$FAVICON dist/$FAVICON
+cp $SRC_FOLDER/$PROFILE dist/$PROFILE
 
 echo "Build complete!"
