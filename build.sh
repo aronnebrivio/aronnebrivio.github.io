24822547e56ae6465e6d05d96d19b65ed20558db
Aronne Brivio <aronne.brivio@gmail.com>
2020-05-04 19:52:05 +0200

    Refactoring codebase with scripts folder

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+MAIN_FILE_NAME="index"
 SRC_FOLDER="src"
 DIST_FOLDER="dist"
-MAIN_FILE_NAME="index"
+SCRIPTS_FOLDER="scripts"
 PALETTE="styles/palette.css"
 THEME="styles/theme.css"
-ZENSCROLL="zenscroll-min.js"
 FAVICON="favicon.png"
 PROFILE="profile.jpg"
 NOJEKYLL=".nojekyll"
@@ -24,7 +24,7 @@ echo "Building..."
 ./node_modules/.bin/cssnano $SRC_FOLDER/$MAIN_FILE_NAME.css $DIST_FOLDER/$MAIN_FILE_NAME.css
 cp $SRC_FOLDER/$FAVICON $DIST_FOLDER/$FAVICON
 cp $SRC_FOLDER/$PROFILE $DIST_FOLDER/$PROFILE
-cp $SRC_FOLDER/$ZENSCROLL $DIST_FOLDER/$ZENSCROLL
+cp -r $SRC_FOLDER/$SCRIPTS_FOLDER $DIST_FOLDER/$SCRIPTS_FOLDER
 cp $NOJEKYLL $DIST_FOLDER/$NOJEKYLL
 cp $CNAME $DIST_FOLDER/$CNAME
 
